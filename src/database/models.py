f30cbe176cfed13a7b65d4c402007682ad34f95a
NickGoit <113237474+NickGoit@users.noreply.github.com>
2023-04-08 12:04:11 +0300

    update DB, add user table, FK to contact table

--- src/database/models.py
+++ src/database/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, Integer, String, func, DATE, DateTime
+from sqlalchemy import Column, Integer, String, func, DATE, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -15,6 +15,15 @@ class Contact(Base):
     date_of_birth = Column(DATE)
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    user_id = Column('user_id', ForeignKey('users.id', ondelete='CASCADE'), default=None)
+    user = relationship('User', backref="contact")
 
 
-
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    created_at = Column('crated_at', DateTime, default=func.now())
+    refresh_token = Column(String(255), nullable=True)
